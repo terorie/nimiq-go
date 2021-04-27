dc788528c3c44840babbafba86bb40c1c9ea7db3
Richard Patel <me@terorie.dev>
2021-04-27 21:41:58 +0200

    Upgrade deps

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module terorie.dev/nimiq
 
-go 1.15
+go 1.16
 
 require (
-	github.com/pelletier/go-toml v1.8.1
+	github.com/pelletier/go-toml v1.9.0
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
 )
--- tree/store_mem.go
+++ tree/store_mem.go
@@ -17,17 +17,14 @@ func NewMemStore() *MemStore {
 // GetNode returns a node previously inserted with PutNode.
 func (m *MemStore) GetNode(nbs Nibbles) Node {
 	return m.nodes[string(nbs)]
-	//return m.nodes[nbs.String()]
 }
 
 // PutNode sets a node under the path described by Nibbles.
 func (m *MemStore) PutNode(nbs Nibbles, node Node) {
 	m.nodes[string(nbs)] = node
-	//m.nodes[nbs.String()] = node
 }
 
 // DelNode removes an existing node or does nothing when the node did not exist.
 func (m *MemStore) DelNode(nbs Nibbles) {
 	delete(m.nodes, string(nbs))
-	//delete(m.nodes, nbs.String())
 }
